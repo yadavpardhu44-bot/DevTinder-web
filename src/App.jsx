958191266ebe80c92bc4df2702fde4c99ab86f20
Pardhu Yadav <yadavpardhu44@gmail.com>
2026-07-18 09:16:33 +0530

    tailwind + daisyui + navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
+import NavBar from "./components/NavBar";
 function App() {
- 
   return (
     <>
-      <div>
-        <h1>Hello world</h1>
-      </div>
+      <NavBar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
