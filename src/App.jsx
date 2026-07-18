8ecab88ce38319fc474a8e2e89f3f8cb764cc0f1
Pardhu Yadav <yadavpardhu44@gmail.com>
2026-07-18 15:58:19 +0530

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import NavBar from "./components/NavBar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 function App() {
   return (
     <>
-      <NavBar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
