fc639ecb52539d886b452c2a5d3f2459ea0b53ec
Pardhu Yadav <yadavpardhu44@gmail.com>
2026-08-06 22:41:51 +0530

    made BASE_URL dynamic

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,5 @@
-export const BASE_URL = "/api";
+//Production
+// export const BASE_URL = "/api";
+
+//Dev
+export const BASE_URL = location.hostname === "localhost" ? "http://localhost:7777" : "/api";
